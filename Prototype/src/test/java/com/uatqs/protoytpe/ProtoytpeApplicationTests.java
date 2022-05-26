9d39c2f47138a05a39da34d01e88eab5e860bec2
Pedro Jorge <pedro.simao10@ua.pt>
2022-05-26 12:51:28 +0100

    UI Prototyping underway

---
+++ Prototype/src/test/java/com/uatqs/protoytpe/ProtoytpeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uatqs.protoytpe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtoytpeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
