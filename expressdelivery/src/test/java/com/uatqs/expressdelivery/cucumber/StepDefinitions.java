be702266be6c0aa8a3af3ea541b0f7bdce63e922
Pedro Jorge <pedro.simao10@ua.pt>
2022-06-08 18:38:39 +0100

    cucumber test changes

--- expressdelivery/src/test/java/com/uatqs/expressdelivery/cucumber/StepDefinitions.java
+++ expressdelivery/src/test/java/com/uatqs/expressdelivery/cucumber/StepDefinitions.java
@@ -2,6 +2,7 @@ package com.uatqs.expressdelivery.cucumber;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 import io.cucumber.java.After;
 import io.cucumber.java.en.And;
@@ -10,47 +11,44 @@ import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class StepDefinitions {
 
     private WebDriver webDriver;
-    
+
     @When("I navigate to {string}")
     public void i_navigate_to(String url) {
-        webDriver = WebDriverManager.chromedriver().create();
+        webDriver = new HtmlUnitDriver();
         webDriver.get(url);
     }
-    @When("I click on the Login button")
-    public void i_click_on_the_login_button() {
-        // Write code here that turns the phrase above into concrete actions
-    }
     @When("I set the Email as {string}")
-    public void i_set_the_email_as(String string) {
-        // Write code here that turns the phrase above into concrete actions
+    public void i_set_the_email_as(String email) {
+        webDriver.findElement(By.id("email")).sendKeys(email);
     }
     @When("I set the Password as {string}")
-    public void i_set_the_password_as(String string) {
-        // Write code here that turns the phrase above into concrete actions
+    public void i_set_the_password_as(String password) {
+        webDriver.findElement(By.id("password")).sendKeys(password);
     }
     @When("I click on the {string} button")
-    public void i_click_on_the_button(String string) {
-        // Write code here that turns the phrase above into concrete actions
+    public void i_click_on_the_button(String login) {
+        webDriver.findElement(By.id("loginSubmit")).click();
     }
 
     @When("I click on the {string} button on the side bar")
-    public void i_click_on_the_button_on_the_side_bar(String string) {
-        // Write code here that turns the phrase above into concrete actions
+    public void i_click_on_the_button_on_the_side_bar(String dashboard) {
+        webDriver.findElement(By.linkText("Riders Performance")).click();
     }
 
     @Then("I should see the message {string}")
     public void i_should_see_the_message(String string) {
-        // Write code here that turns the phrase above into concrete actions
+        assertThat(webDriver.findElement(By.xpath(".//div[@class='navbar-menu-wrapper d-flex align-items-center']/form[@class='ml-auto search-form d-none d-md-block']/div[@class='form-group']")).getText(), containsString(string));
     }
 
     @Then("I should see a table with the names of the riders, such as {string}")
-    public void i_should_see_a_table_with_the_names_of_the_riders_such_as(String string) {
-        // Write code here that turns the phrase above into concrete actions
+    public void i_should_see_a_table_with_the_names_of_the_riders_such_as(String rider) {
+        assertThat(webDriver.findElement(By.xpath("/html/body/div[2]/div/div/div/div/div[1]/div/div/div[2]/div[2]/span")).getText()).isEqualTo(rider);
     }
 
     @After()
